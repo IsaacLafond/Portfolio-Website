b464288cc303d9bb94d0e357625c02ed5630d3a6
Isaac Lafond <isaaclafond@Isaacs-MacBook-Pro.local>
2025-11-12 21:16:45 -0500

    [feat] Unstyled content - Add about, experience, and projects pages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+// Base imports
 import './style.css'
+
+// Page imports
+import AboutPage from './pages/about.html?raw'
+import ProjectsPage from './pages/projects.html?raw'
+import ExperiencePage from './pages/experience.html?raw'
+
+// Component imports
 import { TerminalHeader } from "./components/header/header";
 import { AsciiTreeMenu } from "./components/asciiTreeMenu/asciiTreeMenu";
+
+// Asset imports
 import topojpeg from "./assets/TopoBackground.jpg";
 
-// require webcomponents
+
+// def webcomponent custom elems
 customElements.define("terminal-header", TerminalHeader)
 customElements.define("ascii-tree-menu", AsciiTreeMenu)
 
@@ -19,23 +30,40 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     align-items: center;
     justify-content: center;
     background: black url(${topojpeg}) no-repeat center/cover;">
+
     <div id="terminal-window" style="
         height: 97vh;
         width: 97vw;
         overflow: hidden;
         border-radius: 15px;
         border: solid 1px #5B5B5B;">
+
         <terminal-header data-working-dir='isaaclafond' data-dimensions='420x69'></terminal-header>
+        
         <div style="position: relative; display: flex; align-items: center; justify-content: center; height: calc(100% - 30px); width: 100%; padding: 5px; background-color: #20202A;">
             
-            <div id="terminal" style="height: 100%; width: 100%;"></div>
+            <div id="terminal" style="height: 100%; width: 100%; overflow: scroll;">
+
+                <section>${AboutPage}</section>
+                <br>
+
+                <section>
+                    ${ProjectsPage}
+                    <ascii-tree-menu style="color: #C6D0E9;"></ascii-tree-menu>
+                </section>
+                <br>
+                
+                <section>${ExperiencePage}</section>
+                <br>
+                
+            </div>
+            
         </div>
+            
     </div>
+            
 </div>
 `
-            // <overlay-menu data-display='block' style="background-color: #20202A;">
-            //     <ascii-tree-menu style="color: #C6D0E9;"></ascii-tree-menu>
-            // </overlay-menu>
 
 // setupTerminal()
 
